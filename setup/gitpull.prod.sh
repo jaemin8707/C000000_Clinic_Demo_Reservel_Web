5e7ae887ed3fc4d03dd8c220fa87c7fcee449ca9
User Itc-dev <itc-dev@www3598.sakura.ne.jp>
2019-05-10 17:01:05 +0900

    crontabとpull設定ファイル

--- setup/gitpull.prod.sh
+++ setup/gitpull.prod.sh
@@ -1,17 +1,17 @@
-cd ~/laravel_project/Reservel_akatsuki/reservel-web
+cd ~/laravel_project/Reservel_otake/reservel-web
 php artisan down #メンテモード
-cd ~/laravel_project/Reservel_akatsuki/reservel-admin
+cd ~/laravel_project/Reservel_otake/reservel-admin
 php artisan down #メンテモード
 
 cd  ~/laravel_project/
-cp -r  ~/laravel_project/Reservel_akatsuki ./Reservel_akatsuki_`date +\%Y\%m\%d%H%M`
+cp -r  ~/laravel_project/Reservel_otake ./Reservel_otake_`date +\%Y\%m\%d%H%M`
 
-cd  ~/laravel_project/Reservel_akatsuki
+cd  ~/laravel_project/Reservel_otake
 git pull 
 
-cd  ~/laravel_project/Reservel_akatsuki/reservel-web
+cd  ~/laravel_project/Reservel_otake/reservel-web
 php artisan up #通常モード
-cd  ~/laravel_project/Reservel_akatsuki/reservel-admin
+cd  ~/laravel_project/Reservel_otake/reservel-admin
 php artisan up #通常???ード
 
 
