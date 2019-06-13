34486fa252b8c9a87483bf2b53385b68f3fbf4a0
User Itc-dev <itc-dev@www3598.sakura.ne.jp>
2019-06-13 16:10:10 +0900

    本番用pull設定修正

--- setup/gitpull.prod.sh
+++ setup/gitpull.prod.sh
@@ -1,13 +1,23 @@
+cd  ~/laravel_project/Reservel_otake/
+./clearcache.sh
+
 cd ~/laravel_project/Reservel_otake/reservel-web
 php artisan down #メンテモード
 cd ~/laravel_project/Reservel_otake/reservel-admin
 php artisan down #メンテモード
 
+echo "Backup start"
 cd  ~/laravel_project/
 cp -r  ~/laravel_project/Reservel_otake ./Reservel_otake_`date +\%Y\%m\%d%H%M`
 
 cd  ~/laravel_project/Reservel_otake
+echo "pull Start"
 git pull 
+echo "git pull done."
+
+cd  ~/laravel_project/Reservel_otake/reservel-web
+echo "Migrate start"
+php artisan migrate --env=prod
 
 cd  ~/laravel_project/Reservel_otake/reservel-web
 php artisan up #通常モード
