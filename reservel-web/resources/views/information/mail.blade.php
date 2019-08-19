41a4362b6b8a16925cb6e3542400b8a98c3e7134
User Itc-dev <itc-dev@www3598.sakura.ne.jp>
2019-08-19 15:15:55 +0900

    メール受信案内@yahoo.co.jpに変更

--- reservel-web/resources/views/information/mail.blade.php
+++ reservel-web/resources/views/information/mail.blade.php
@@ -11,7 +11,7 @@
 
 <main class="data_main">
  <p class="data_main_comment">迷惑メールの設定によっては、受付システムからのメールが届かない場合があります。<br>
-  メールが届かない場合は、受付システムからのメールが受信できるようにドメイン指定受信で「@otake-reservel.jp」を許可するように設定してください。</p>
+  メールが届かない場合は、受付システムからのメールが受信できるようにドメイン指定受信で「@yahoo.co.jp」を許可するように設定してください。</p>
 
 	
     <section class="data_inner">	
@@ -36,19 +36,19 @@
 		   
 	  <section class="data_inner">
 	  <div class="data_inner_title">■docomoのメールアドレスをお使いの方</div>
-	  <p class="data_inner_comment">下記のページの「受信リスト設定」を参照いただき、「@otake-reservel.jp」を受信するドメインに追加してください。</p>
+	  <p class="data_inner_comment">下記のページの「受信リスト設定」を参照いただき、「@yahoo.co.jp」を受信するドメインに追加してください。</p>
 	  <a href="https://www.nttdocomo.co.jp/info/spam_mail/spmode/domain/">docomoドメイン指定受信設定方法</a>
 	  </section>
 		   
 	  <section class="data_inner">
 	  <div class="data_inner_title">■auのメールアドレスをお使いの方</div>
-	  <p class="data_inner_comment">下記のページを参照いただき、受信リスト設定で「@otake-reservel.jp」を追加してください。</p>
+	  <p class="data_inner_comment">下記のページを参照いただき、受信リスト設定で「@yahoo.co.jp」を追加してください。</p>
 	  <a href="https://www.au.com/support/service/mobile/trouble/mail/email/filter/detail/domain/">auドメイン指定受信設定方法</a>
 	  </section>
 		   
 	  <section class="data_inner">
 	  <div class="data_inner_title">■SoftBankのメールアドレス（S!メール（MMS）とEメール（i））をお使いの方</div>
-	  <p class="data_inner_comment">下記のページを参照いただき、メール設定で「@otake-reservel.jp」を許可するメールとして登録してください。</p>
+	  <p class="data_inner_comment">下記のページを参照いただき、メール設定で「@yahoo.co.jp」を許可するメールとして登録してください。</p>
 	  <a href="https://www.softbank.jp/mobile/support/iphone/antispam/email_i/white/">SoftBankドメイン指定受信設定方法</a>
 	  </section>
 	
