a9f350ac80879fb3cd4d9f69b3ae7d933ec97481
User Itc-dev <itc-dev@www3598.sakura.ne.jp>
2019-12-03 11:06:37 +0900

    gitpull 修正

--- setup/gitpull.prod.sh
+++ setup/gitpull.prod.sh
@@ -1,27 +1,27 @@
-cd  ~/laravel_project/Reservel_seibozaka/
+cd  ~/laravel_project/Reservel_shinka/
 ./clearcache.sh
 
-cd ~/laravel_project/Reservel_seibozaka/reservel-web
+cd ~/laravel_project/Reservel_shinka/reservel-web
 php artisan down #メンテモード
-cd ~/laravel_project/Reservel_seibozaka/reservel-admin
+cd ~/laravel_project/Reservel_shinka/reservel-admin
 php artisan down #メンテモード
 
 echo "Backup start"
 cd  ~/laravel_project/
-cp -r  ~/laravel_project/Reservel_seibozaka ./Reservel_seibozaka_`date +\%Y\%m\%d%H%M`
+cp -r  ~/laravel_project/Reservel_shinka ./Reservel_shinka_`date +\%Y\%m\%d%H%M`
 
-cd  ~/laravel_project/Reservel_seibozaka
+cd  ~/laravel_project/Reservel_shinka
 echo "pull Start"
 git pull 
 echo "git pull done."
 
-cd  ~/laravel_project/Reservel_seibozaka/reservel-web
+cd  ~/laravel_project/Reservel_shinka/reservel-web
 echo "Migrate start"
 php artisan migrate --env=prod
 
-cd  ~/laravel_project/Reservel_seibozaka/reservel-web
+cd  ~/laravel_project/Reservel_shinka/reservel-web
 php artisan up #通常モード
-cd  ~/laravel_project/Reservel_seibozaka/reservel-admin
+cd  ~/laravel_project/Reservel_shinka/reservel-admin
 php artisan up #通常???ード
 
 
